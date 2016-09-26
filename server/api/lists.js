6252596385d1c4f53df7dd911f7db65d3f9d248c
TJ Walker <tjfwalker+git@gmail.com>
2016-09-26 14:35:49 -0700

    Create the subroute '/lists' and its response

--- server/api/lists.js
+++ server/api/lists.js
@@ -1,4 +1,12 @@
 import express from 'express'
 const router = new express.Router
 
+router.get( '/', ( request, response ) => {
+  response.json( {
+    id: 'lorem',
+    board_id: 'lorem',
+    name: 'lorem'
+  } )
+} )
+
 export default router
