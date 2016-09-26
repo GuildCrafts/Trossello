37f6fcecf57794646b1dc26d3b33f02eba780033
Ethan Stark <ethanstark@gmail.com>
2016-09-26 11:41:05 -0700

    started adding api routes

--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ import bodyParser from 'body-parser'
 import cookieSession from 'cookie-session'
 import errorHandlers from './error_handlers'
 import authRoutes from './auth_routes'
+import apiUsersRouter from './api/users'
 
 const appRoot = process.env.APP_ROOT
 const buildPath = process.env.BUILD_PATH
 const server = express()
+
 module.exports = server
 
 server.set('env', process.env.NODE_ENV)
@@ -26,6 +28,16 @@ server.use('/', authRoutes);
 server.get('/session', (request, response) => {
   response.json(request.session)
 });
+
+server.get('/api/current-user', (request, response) => {
+  response.json({
+    id: 42,
+    email: "me@me.com",
+    password: "123"
+  })
+})
+
+server.use('/api/users', apiUsersRouter)
 
 server.get('/*', (request, response) => {
   response.sendFile(buildPath+'/public/index.html')
