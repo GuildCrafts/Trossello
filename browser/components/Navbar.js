b22855cf999d21743197c537ea7593bff2f23236
Harman Singh <harmanlearns@gmail.com>
2016-09-29 14:23:08 -0700

    styled the logged in navbar

--- browser/components/Navbar.js
+++ browser/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Navbar.sass'
 import PresentationalComponent from './PresentationalComponent'
 import Link from './Link'
+import Icon from './Icon'
 import LogoutButton from './LogoutButton'
 import LoginButton from './LoginButton'
 
@@ -25,17 +26,24 @@ const LoggedOutNavbar = (props) => {
   </div>
 }
 
-const LoggedInNavbar = ({ auth }) => {
+const LoggedInNavbar = ({auth}) => {
   return <div className="Navbar">
-    <div className="Navbar-links">
-      <Link to="/boards">Boards</Link>
+    <button className="Navbar-button BoardButton">Board</button>
+    <input type="text" className="Navbar-SearchInput" ></input>
+    <div className="Navbar-BoardIndexButton">
+      <a href="/">Trossello</a>
     </div>
-    <div className="Navbar-links">
-      <div>{auth.user.name}</div>
-      <LogoutButton>Logout</LogoutButton>
-    </div>
+    <button className="Navbar-button CreateBoardButton">
+      <Icon type="plus" />
+    </button>
+    <button className="Navbar-button Navbar-AvatarButton">
+      <img src={auth.user.avatar_url} />
+      <span>{auth.user.name}</span>
+    </button>
+    <button className="Navbar-button AlertButton">
+      <Icon type="bell" />
+    </button>
   </div>
 }
-
 
 export default PresentationalComponent(Navbar)
