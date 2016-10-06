dd3f85eff68afcb10428fd0e04e627f8c26286c7
Jared Grippe <jared@deadlyicon.com>
2016-10-05 22:18:17 -0700

    added CreateBoardPopover component

---
+++ browser/components/CreateBoardPopover.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import $ from 'jquery'
+import './CreateBoardPopover.sass'
+import Form from './Form'
+
+class CreateBoardPopover extends Component {
+
+  static contextTypes = {
+    redirectTo: React.PropTypes.func,
+  }
+
+  constructor(props){
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  componentDidMount(){
+    this.refs.name.focus()
+  }
+
+  onSubmit(event){
+    event.preventDefault()
+    const board = {
+      name: this.refs.name.value,
+      background_color: this.refs.color.value,
+    }
+    $.ajax({
+      method: "POST",
+      url: '/api/boards',
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      data: JSON.stringify(board),
+    }).then((board) => {
+      this.context.redirectTo('/boards/'+board.id)
+    })
+  }
+
+  render(props){
+    return <div className="CreateBoardPopover">
+      <div className="CreateBoardPopover-header">
+        Create A Board
+        <hr/>
+      </div>
+      <Form onSubmit={this.onSubmit}>
+        <label>
+          <div>Name</div>
+          <input type="text" ref="name"/>
+        </label>
+        <label>
+          <div>Color</div>
+          <input type="text" ref="color" defaultValue="#2E86AB" />
+        </label>
+        <input type="submit" value="Create" />
+      </Form>
+    </div>
+  }
+}
+
+export default CreateBoardPopover
