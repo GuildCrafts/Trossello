e0ce82bcdc8b0fb7ab251d377d909ac1fd905d74
Phillip Lorenzo <pllearns@gmail.com>
2016-10-17 11:05:16 -0700

    WIP: Working on archiving of boards, cards, and lists.

--- server/database/commands.js
+++ server/database/commands.js
@@ -26,6 +26,23 @@ const deleteRecord = (table, id) =>
     .where('id', id)
     .del()
 
+const archiveRecord = (table, id) =>
+  knex
+    .table(table)
+    .where('id', id)
+    .update({
+      archived: true,
+    })
+
+const unarchiveRecord = (table, id) =>
+  knex
+    .table(table)
+    .where('id', id)
+    .update({
+      archived: false,
+    })
+
+
 //
 
 const findOrCreateUserFromGithubProfile = (githubProfile) => {
@@ -81,6 +98,25 @@ const updateCard = (id, attributes) =>
 
 const deleteCard = (id) =>
   deleteRecord('cards', id)
+
+const archiveCard = (id) =>
+  archiveRecord('cards', id)
+
+const unarchiveCard = (id) =>
+  unarchiveRecord('cards', id)
+
+const archiveList = (id) =>
+  archiveRecord('lists', id)
+
+const unarchiveList = (id) =>
+  unarchiveRecord('lists', id)
+
+const archiveBoard = (id) =>
+  archiveRecord('boards', id)
+
+const unarchiveBoard = (id) =>
+  unarchiveRecord('boards', id)
+
 
 //
 
