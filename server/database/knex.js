35dc078b050d6dc95f354593f7f6d212e8e17201
Jared Grippe <jared@deadlyicon.com>
2016-10-19 17:31:31 -0700

    trying to fix sql unique key constraint violation errors on circle ci

--- server/database/knex.js
+++ server/database/knex.js
@@ -6,13 +6,15 @@ const config = require('../../../knexfile')[process.env.NODE_ENV]
 const knex = Knex(config)
 
 knex.truncateAllTables = function(){
-  return Promise.all([
-    knex.table('user_boards').del(),
-    knex.table('cards').del(),
-    knex.table('lists').del(),
-    knex.table('boards').del(),
-    knex.table('users').del(),
-  ])
+  return knex.schema.raw(`
+    BEGIN;
+    TRUNCATE user_boards RESTART IDENTITY CASCADE;
+    TRUNCATE cards       RESTART IDENTITY CASCADE;
+    TRUNCATE lists       RESTART IDENTITY CASCADE;
+    TRUNCATE boards      RESTART IDENTITY CASCADE;
+    TRUNCATE users       RESTART IDENTITY CASCADE;
+    COMMIT;
+  `)
 }
 
 export default knex
