6bc9a549495bca5dea3874ddcbd77cb8606992c9
Amelia Clarke <ameliavoncat@gmail.com>
2016-10-24 18:02:12 -0700

    created MenuSideBar component

--- browser/components/BoardShowPage.js
+++ browser/components/BoardShowPage.js
@@ -11,6 +11,7 @@ import Card from './BoardShowPage/Card'
 import NewListForm from './BoardShowPage/NewListForm'
 import InviteByEmailButton from './InviteByEmailButton'
 import LeaveBoardButton from './BoardShowPage/LeaveBoardButton'
+import MenuSideBarToggle from './BoardShowPage/MenuSideBarToggle'
 
 class BoardProvider extends Component {
   constructor(props){
@@ -225,9 +226,7 @@ class BoardShowPage extends React.Component {
       <div className="BoardShowPage-Header">
         <h1>{board.name}</h1>
         <div>
-          <DownloadBoardButton boardId={board.id}/>
-          <InviteByEmailButton boardId={board.id}/>
-          <LeaveBoardButton boardId={board.id}/>
+          <MenuSideBarToggle board={board} />
         </div>
       </div>
       <div
