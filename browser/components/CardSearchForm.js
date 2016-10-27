fb5be6e096d58265156da98159fb06edabe5719f
Jared Grippe <jared@deadlyicon.com>
2016-10-27 15:20:39 -0700

    renamed CardViewModal to CardModal

--- browser/components/CardSearchForm.js
+++ browser/components/CardSearchForm.js
@@ -5,7 +5,6 @@ import Link from './Link'
 import Icon from './Icon'
 import $ from 'jquery'
 import Card from './BoardShowPage/Card'
-import CardViewModal from './CardViewModal'
 
 export default class CardSearchForm extends Component {
 
