39338a223c3125eada2938963dc178f6d80ac350
anasauce <anasauceda1843@gmail.com>
2016-10-27 14:02:54 -0700

    Implemented Basic Style as well as added <Link></Link> and <Button></Button> components

--- browser/components/Link.js
+++ browser/components/Link.js
@@ -1,4 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from 'simple-react-router'
+import './Link.sass'
 
-export default Link
+class TypedLink extends Component {
+  static propTypes = {
+    type: React.PropTypes.string,
+    href: React.PropTypes.string,
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    const props = Object.assign({}, this.props)
+    props.type = this.props.type || 'default'
+    props.href = props.href || props.to || ''
+    props.onClick = props.onClick || null
+    props.className = `Link Link-${props.type} ${props.className||''}`
+    return <Link { ...props }>{ this.props.children }</Link>
+  }
+}
+
+
+export default TypedLink
