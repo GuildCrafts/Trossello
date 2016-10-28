a4b5c997868f7841ad5ccdfc9a7050f0f848752d
Jared Grippe <jared@deadlyicon.com>
2016-10-28 13:00:10 -0700

    ConfirmationDialog now uses Button

--- browser/components/ConfirmationDialog.js
+++ browser/components/ConfirmationDialog.js
@@ -1,9 +1,10 @@
+import './ConfirmationDialog.sass'
 import React, { Component } from 'react'
 import Link from './Link'
 import Icon from './Icon'
-import './ConfirmationDialog.sass'
 import ModalView from './ModalView'
 import DialogBox from './DialogBox'
+import Button from './Button'
 
 export default class ConfirmationDialog extends Component {
   static propTypes = {
@@ -15,7 +16,7 @@ export default class ConfirmationDialog extends Component {
   }
 
   componentDidMount(){
-    this.refs.cancel.refs.link.focus()
+    this.refs.cancel.refs.button.focus()
   }
 
   render(){
@@ -24,12 +25,12 @@ export default class ConfirmationDialog extends Component {
       <DialogBox onClose={this.props.onAbort} heading={this.props.title}>
         <h4>{this.props.message}</h4>
         <div className="ConfirmationDialog-controls">
-          <Link ref="cancel" className="ConfirmationDialog-controls-button ConfirmationDialog-controls-cancel" onClick={this.props.onAbort}>
+          <Button ref="cancel" className="ConfirmationDialog-controls-button ConfirmationDialog-controls-cancel" onClick={this.props.onAbort}>
             Cancel
-          </Link>
-          <Link className="ConfirmationDialog-controls-button ConfirmationDialog-controls-ok" onClick={this.props.onConfirm}>
+          </Button>
+          <Button type="danger" onClick={this.props.onConfirm}>
             {this.props.buttonName}
-          </Link>
+          </Button>
         </div>
       </DialogBox>
     </ModalView>
