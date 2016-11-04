455133a92734d522f7715af46274a3c66dab4f6d
Ethan Stark <ethanstark@gmail.com>
2016-11-04 13:56:39 -0700

    Polished card edit modal

--- browser/components/Icon.js
+++ browser/components/Icon.js
@@ -6,7 +6,7 @@ const Sizes = ['fa-sm','','fa-lg','fa-2x','fa-3x','fa-4x','fa-5x']
 const Icon = ({className, children, type, size}) => {
   size = Number(size)
   if (isNaN(size)) size = 1
-  className = `fa fa-${type} ${Sizes[size]} ${className||''}`
+  className = `Icon fa fa-${type} ${Sizes[size]} ${className||''}`
   return <i className={className}>{children}</i>
 }
 
