b50a98712cb49fa830f35e7fe5afe1e9a5c23e48
Jared Grippe <jared@deadlyicon.com>
2016-11-10 11:12:43 -0800

    the withBoardsListsAndCardsInTheDatabase test helper now uses the withTwoUsersInTheDatabase

--- test/server/api/boards_test.js
+++ test/server/api/boards_test.js
@@ -6,257 +6,254 @@ const{
 } = require('../../helpers')
 
 describe('/api/boards', () => {
+  withBoardsListsAndCardsInTheDatabase(() => {
 
-  withTwoUsersInTheDatabase(() => {
-    withBoardsListsAndCardsInTheDatabase(() => {
+    context('when not logged in', () => {
 
-      context('when not logged in', () => {
-
-        // INDEX
-        describe('GET /api/boards', () => {
-          it('should render 400 Not Authorized', () => {
-            return request('get', '/api/boards')
-              .then(response => {
-                expect(response).to.have.status(400)
-              })
-          })
+      // INDEX
+      describe('GET /api/boards', () => {
+        it('should render 400 Not Authorized', () => {
+          return request('get', '/api/boards')
+            .then(response => {
+              expect(response).to.have.status(400)
+            })
         })
+      })
 
-        // CREATE
-        describe('POST /api/boards', () => {
-          it('should render 400 Not Authorized', () => {
-            return request('post', '/api/boards', {})
-              .then( response => {
-                expect(response).to.have.status(400)
-              })
-          })
+      // CREATE
+      describe('POST /api/boards', () => {
+        it('should render 400 Not Authorized', () => {
+          return request('post', '/api/boards', {})
+            .then( response => {
+              expect(response).to.have.status(400)
+            })
         })
+      })
 
-        // SHOW
-        describe('GET /api/boards/:boardId', () => {
-          it('should render 400 Not Authorized', () => {
-            return request('get', '/api/boards/1')
-              .then(response => {
-                expect(response).to.have.status(400)
-              })
-          })
+      // SHOW
+      describe('GET /api/boards/:boardId', () => {
+        it('should render 400 Not Authorized', () => {
+          return request('get', '/api/boards/1')
+            .then(response => {
+              expect(response).to.have.status(400)
+            })
         })
+      })
 
-        // UPDATE
-        describe('POST /api/boards/:boardId', () => {
-          it('should render 400 Not Authorized', () => {
-            return request('post', '/api/boards/1')
-              .then(response => {
-                expect(response).to.have.status(400)
-              })
-          })
+      // UPDATE
+      describe('POST /api/boards/:boardId', () => {
+        it('should render 400 Not Authorized', () => {
+          return request('post', '/api/boards/1')
+            .then(response => {
+              expect(response).to.have.status(400)
+            })
         })
+      })
 
-        // DELETE
-        describe('POST /api/boards/:boardId/delete', () => {
-          it('should render 400 Not Authorized', () => {
-            return request('post', '/api/boards/101/delete')
-              .then(response => {
-                expect(response).to.have.status(400)
-              })
-          })
+      // DELETE
+      describe('POST /api/boards/:boardId/delete', () => {
+        it('should render 400 Not Authorized', () => {
+          return request('post', '/api/boards/101/delete')
+            .then(response => {
+              expect(response).to.have.status(400)
+            })
         })
+      })
 
-        // CREATE LIST
-        describe('POST /api/boards/:boardId/lists', () => {
-          it('should render 400 Not Authorized', () => {
-            return request('post', '/api/boards/101/lists', {})
-              .then(response => {
-                expect(response).to.have.status(400)
-              })
-          })
+      // CREATE LIST
+      describe('POST /api/boards/:boardId/lists', () => {
+        it('should render 400 Not Authorized', () => {
+          return request('post', '/api/boards/101/lists', {})
+            .then(response => {
+              expect(response).to.have.status(400)
+            })
         })
+      })
 
-        // CREATE BOARD
-        describe('POST /api/boards/:boardId/lists/:listId/cards', () => {
-          it('should render 400 Not Authorized', () => {
-            return request('post', '/api/boards/101/lists/1/cards', {})
-              .then(response => {
-                expect(response).to.have.status(400)
-              })
+      // CREATE BOARD
+      describe('POST /api/boards/:boardId/lists/:listId/cards', () => {
+        it('should render 400 Not Authorized', () => {
+          return request('post', '/api/boards/101/lists/1/cards', {})
+            .then(response => {
+              expect(response).to.have.status(400)
+            })
+        })
+      })
+    })
+
+
+    context('when logged in', () => {
+      beforeEach(() => {
+        return loginAs(1455)
+      })
+
+      // INDEX
+      describe('GET /api/boards', () => {
+        it('should render each board as a JSON array', () => {
+          return request('get', '/api/boards').then(response => {
+            expect(response).to.have.status(200)
+            expect(response.body).to.be.an('array')
+            expect(response.body.length).to.eql(3)
+            const boardNames = response.body.map(board => board.name).sort()
+            expect(boardNames).to.eql(['Board1','Board2', 'Board3'])
           })
         })
       })
 
-
-      context('when logged in', () => {
-        beforeEach(() => {
-          return loginAs(1455)
-        })
-
-        // INDEX
-        describe('GET /api/boards', () => {
-          it('should render each board as a JSON array', () => {
-            return request('get', '/api/boards').then(response => {
-              expect(response).to.have.status(200)
-              expect(response.body).to.be.an('array')
-              expect(response.body.length).to.eql(3)
-              const boardNames = response.body.map(board => board.name).sort()
-              expect(boardNames).to.eql(['Board1','Board2', 'Board3'])
-            })
+      // CREATE
+      describe('POST /api/boards', () => {
+        it('should create a new board and return its json', () => {
+          const boardAttributes = {
+            name: 'Fresh Board'
+          }
+          return request('post', '/api/boards', boardAttributes).then( response => {
+            expect(response).to.have.status(200)
+            expect(response.body.name).to.eql('Fresh Board')
+            expect(response.body.background_color).to.eql('#0079bf')
           })
         })
+      })
 
-        // CREATE
-        describe('POST /api/boards', () => {
-          it('should create a new board and return its json', () => {
-            const boardAttributes = {
-              name: 'Fresh Board'
-            }
-            return request('post', '/api/boards', boardAttributes).then( response => {
-              expect(response).to.have.status(200)
-              expect(response.body.name).to.eql('Fresh Board')
-              expect(response.body.background_color).to.eql('#0079bf')
-            })
+      // SHOW
+      describe('GET /api/boards/<existing board id>', () => {
+        it('should return a null body and a 404 status', () => {
+          return request('get', '/api/boards/101').then(response => {
+            expect(response).to.have.status(200)
+            expect(response.body.id).to.eql(101)
+            expect(response.body.name).to.eql('Board1')
+            expect(response.body.background_color).to.eql('orange')
           })
         })
-
-        // SHOW
-        describe('GET /api/boards/<existing board id>', () => {
-          it('should return a null body and a 404 status', () => {
-            return request('get', '/api/boards/101').then(response => {
-              expect(response).to.have.status(200)
+        context('when download=1', () => {
+          it('should set the xheader', () => {
+            return request('get', '/api/boards/101?download=1').then(response => {                expect(response).to.have.status(200)
+              expect(response).to.have.header('Content-Disposition', 'attachment; filename="board101.json"');
+              expect(response).to.have.header('Content-Type', 'application/json; charset=utf-8');
               expect(response.body.id).to.eql(101)
               expect(response.body.name).to.eql('Board1')
               expect(response.body.background_color).to.eql('orange')
             })
           })
-          context('when download=1', () => {
-            it('should set the xheader', () => {
-              return request('get', '/api/boards/101?download=1').then(response => {                expect(response).to.have.status(200)
-                expect(response).to.have.header('Content-Disposition', 'attachment; filename="board101.json"');
-                expect(response).to.have.header('Content-Type', 'application/json; charset=utf-8');
-                expect(response.body.id).to.eql(101)
-                expect(response.body.name).to.eql('Board1')
-                expect(response.body.background_color).to.eql('orange')
-              })
-            })
-          })
-
         })
 
-        // SHOW
-        describe('GET /api/boards/<non-existant board id>', () => {
-          it('should return a null body and a 404 status', () => {
-            return request('get', '/api/boards/6667').then(response => {
-              expect(response).to.have.status(404)
-              expect(response.body).to.be.null
-            })
-          })
-        })
+      })
 
-        // UPDATE
-        describe('POST /api/boards/:boardId', () => {
-          it('should update the board', () => {
-            const boardAttributes = {
-              name: 'fresh board',
-              starred: true
-            }
-            return request('post', '/api/boards/102', boardAttributes).then(response => {
-              expect(response).to.have.status(200)
-              expect(response.body.id).to.eql(102)
-              expect(response.body.name).to.eql('fresh board')
-              expect(response.body.background_color).to.eql('purple')
-              expect(response.body.starred).to.eql(true)
-            })
-          })
-        })
-
-        // ARCHIVE
-        describe('POST /api/boards/<existing board id>/archive', () => {
-          it('should archive a board and render status 200', () => {
-            return request('post', '/api/boards/102/archive')
-              .then(response => {
-                expect(response).to.have.status(200)
-              })
-              .then( () => request('get', '/api/boards/102'))
-              .then(response => {
-                expect(response).to.have.status(200)
-                expect(response.body.archived).to.eql(true)
-              })
-          })
-        })
-
-        // ARCHIVE
-        describe('POST /api/boards/<non-existant board id>/archive', () => {
-          it('should return a null JSON object and a 404 error', () => {
-            return request('post', '/api/boards/52/archive').then(response => {
-              expect(response).to.have.status(404)
-            })
-          })
-        })
-
-        // Star Board
-        describe('POST /api/boards/<existing board id>/star', () => {
-          it('should set starred to true and return 200', () => {
-            return request('post', '/api/boards/102/star')
-              .then(response => {
-                expect(response).to.have.status(200)
-              })
-              .then( () => request('get', '/api/boards/102'))
-              .then(response => {
-                expect(response).to.have.status(200)
-                expect(response.body.starred).to.eql(true)
-              })
-          })
-        })
-
-        // Unstar Board
-        describe('POST /api/boards/<existing board id>/unstar', () => {
-          it('should set starred to false and return 200', () => {
-            return request('post', '/api/boards/103/unstar')
-              .then(response => {
-                expect(response).to.have.status(200)
-              })
-              .then( () => request('get', '/api/boards/103'))
-              .then(response => {
-                expect(response).to.have.status(200)
-                expect(response.body.starred).to.eql(false)
-              })
-          })
-        })
-
-
-        // CREATE LIST
-        describe('POST /api/boards/:boardId/lists', () => {
-          it('should create a list for that board and render it as json', () => {
-            const listAttributes = {
-              name: 'Things to burn'
-            }
-            return request('post', '/api/boards/101/lists', listAttributes)
-              .then(response => {
-                expect(response).to.have.status(200)
-                expect(response.body.id).to.be.a('number')
-                expect(response.body.board_id).to.eql(101)
-                expect(response.body.name).to.eql('Things to burn')
-              })
-          })
-        })
-
-        // CREATE BOARD
-        describe('POST /api/boards/:boardId/lists/:listId/cards', () => {
-          it('should create a card for that list and board and render it as json', () => {
-            const cardAttributes = {
-              content: 'Old Passport'
-            }
-            return request('post', '/api/boards/101/lists/40/cards', cardAttributes)
-              .then(response => {
-                expect(response).to.have.status(200)
-                expect(response.body.id).to.be.a('number')
-                expect(response.body.board_id).to.eql(101)
-                expect(response.body.list_id).to.eql(40)
-                expect(response.body.content).to.eql('Old Passport')
-              })
+      // SHOW
+      describe('GET /api/boards/<non-existant board id>', () => {
+        it('should return a null body and a 404 status', () => {
+          return request('get', '/api/boards/6667').then(response => {
+            expect(response).to.have.status(404)
+            expect(response.body).to.be.null
           })
         })
       })
 
+      // UPDATE
+      describe('POST /api/boards/:boardId', () => {
+        it('should update the board', () => {
+          const boardAttributes = {
+            name: 'fresh board',
+            starred: true
+          }
+          return request('post', '/api/boards/102', boardAttributes).then(response => {
+            expect(response).to.have.status(200)
+            expect(response.body.id).to.eql(102)
+            expect(response.body.name).to.eql('fresh board')
+            expect(response.body.background_color).to.eql('purple')
+            expect(response.body.starred).to.eql(true)
+          })
+        })
+      })
+
+      // ARCHIVE
+      describe('POST /api/boards/<existing board id>/archive', () => {
+        it('should archive a board and render status 200', () => {
+          return request('post', '/api/boards/102/archive')
+            .then(response => {
+              expect(response).to.have.status(200)
+            })
+            .then( () => request('get', '/api/boards/102'))
+            .then(response => {
+              expect(response).to.have.status(200)
+              expect(response.body.archived).to.eql(true)
+            })
+        })
+      })
+
+      // ARCHIVE
+      describe('POST /api/boards/<non-existant board id>/archive', () => {
+        it('should return a null JSON object and a 404 error', () => {
+          return request('post', '/api/boards/52/archive').then(response => {
+            expect(response).to.have.status(404)
+          })
+        })
+      })
+
+      // Star Board
+      describe('POST /api/boards/<existing board id>/star', () => {
+        it('should set starred to true and return 200', () => {
+          return request('post', '/api/boards/102/star')
+            .then(response => {
+              expect(response).to.have.status(200)
+            })
+            .then( () => request('get', '/api/boards/102'))
+            .then(response => {
+              expect(response).to.have.status(200)
+              expect(response.body.starred).to.eql(true)
+            })
+        })
+      })
+
+      // Unstar Board
+      describe('POST /api/boards/<existing board id>/unstar', () => {
+        it('should set starred to false and return 200', () => {
+          return request('post', '/api/boards/103/unstar')
+            .then(response => {
+              expect(response).to.have.status(200)
+            })
+            .then( () => request('get', '/api/boards/103'))
+            .then(response => {
+              expect(response).to.have.status(200)
+              expect(response.body.starred).to.eql(false)
+            })
+        })
+      })
+
+
+      // CREATE LIST
+      describe('POST /api/boards/:boardId/lists', () => {
+        it('should create a list for that board and render it as json', () => {
+          const listAttributes = {
+            name: 'Things to burn'
+          }
+          return request('post', '/api/boards/101/lists', listAttributes)
+            .then(response => {
+              expect(response).to.have.status(200)
+              expect(response.body.id).to.be.a('number')
+              expect(response.body.board_id).to.eql(101)
+              expect(response.body.name).to.eql('Things to burn')
+            })
+        })
+      })
+
+      // CREATE BOARD
+      describe('POST /api/boards/:boardId/lists/:listId/cards', () => {
+        it('should create a card for that list and board and render it as json', () => {
+          const cardAttributes = {
+            content: 'Old Passport'
+          }
+          return request('post', '/api/boards/101/lists/40/cards', cardAttributes)
+            .then(response => {
+              expect(response).to.have.status(200)
+              expect(response.body.id).to.be.a('number')
+              expect(response.body.board_id).to.eql(101)
+              expect(response.body.list_id).to.eql(40)
+              expect(response.body.content).to.eql('Old Passport')
+            })
+        })
+      })
     })
 
   })
+
 })
--- test/server/api/cards_test.js
+++ test/server/api/cards_test.js
@@ -6,72 +6,70 @@ const{
 } = require('../../helpers')
 
 describe('/api/cards', () => {
-  withTwoUsersInTheDatabase(() => {
-    withBoardsListsAndCardsInTheDatabase(() => {
+  withBoardsListsAndCardsInTheDatabase(() => {
 
-      context('when not logged in', () => {
-        // UPDATE
-        describe('POST /api/cards/:cardId', () => {
-          it('should render 400 Not Authorized', () => {
-            return request('post', '/api/cards/12')
-              .then(response => {
-                expect(response).to.have.status(400)
-              })
-          })
-        })
-
-        // DELETE
-        describe('POST /api/cards/:cardId/archive', () => {
-          it('should render 400 Not Authorized', () => {
-            return request('post', '/api/cards/12/archive')
-              .then(response => {
-                expect(response).to.have.status(400)
-              })
-          })
+    context('when not logged in', () => {
+      // UPDATE
+      describe('POST /api/cards/:cardId', () => {
+        it('should render 400 Not Authorized', () => {
+          return request('post', '/api/cards/12')
+            .then(response => {
+              expect(response).to.have.status(400)
+            })
         })
       })
 
-
-      context('when logged in', () => {
-        beforeEach(() => {
-          return loginAs(1455)
+      // DELETE
+      describe('POST /api/cards/:cardId/archive', () => {
+        it('should render 400 Not Authorized', () => {
+          return request('post', '/api/cards/12/archive')
+            .then(response => {
+              expect(response).to.have.status(400)
+            })
         })
-
-        // UPDATE
-        describe('POST /api/cards/:cardId', () => {
-          it('should update the card and render it as json', () => {
-            const cardAttributes = {
-              content: 'Eat some shoe laces'
-            }
-            return request('post', '/api/cards/83', cardAttributes)
-              .then(response => {
-                expect(response).to.have.status(200)
-                expect(response.body.id).to.eql(83)
-                expect(response.body.list_id).to.eql(41)
-                expect(response.body.content).to.eql('Eat some shoe laces')
-              })
-          })
-        })
-
-        // DELETE
-        describe('POST /api/cards/:cardId/archive', () => {
-          it('should archive the card and render null', () => {
-            return queries.getCardById(83)
-              .then(card => {
-                expect(card.archived).to.eql(false)
-              })
-              .then(() => request('post', '/api/cards/83/archive'))
-              .then(response => {
-                expect(response).to.have.status(200)
-              })
-              .then(() => queries.getCardById(83))
-              .then(card => {
-                expect(card.archived).to.eql(true)
-              })
-          })
-        })
-
       })
+    })
+
+
+    context('when logged in', () => {
+      beforeEach(() => {
+        return loginAs(1455)
+      })
+
+      // UPDATE
+      describe('POST /api/cards/:cardId', () => {
+        it('should update the card and render it as json', () => {
+          const cardAttributes = {
+            content: 'Eat some shoe laces'
+          }
+          return request('post', '/api/cards/83', cardAttributes)
+            .then(response => {
+              expect(response).to.have.status(200)
+              expect(response.body.id).to.eql(83)
+              expect(response.body.list_id).to.eql(41)
+              expect(response.body.content).to.eql('Eat some shoe laces')
+            })
+        })
+      })
+
+      // DELETE
+      describe('POST /api/cards/:cardId/archive', () => {
+        it('should archive the card and render null', () => {
+          return queries.getCardById(83)
+            .then(card => {
+              expect(card.archived).to.eql(false)
+            })
+            .then(() => request('post', '/api/cards/83/archive'))
+            .then(response => {
+              expect(response).to.have.status(200)
+            })
+            .then(() => queries.getCardById(83))
+            .then(card => {
+              expect(card.archived).to.eql(true)
+            })
+        })
+      })
+
     })
   })
 })
