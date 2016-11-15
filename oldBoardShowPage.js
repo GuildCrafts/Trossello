299bc2387dbbc6b35b8f916f0958ed357051da87
Max Antinone <mantinone@gmail.com>
2016-11-14 17:58:36 -0800

    Added temporary reference files

---
+++ oldBoardShowPage.js
@@ -0,0 +1,287 @@
+
+import knex from './knex'
+import queries from './queries'
+import { sendWelcomeEmail } from '../mail/mailer'
+
+const firstRecord = records => records[0]
+
+const createRecord = (table, attributes) =>
+  knex
+    .table(table)
+    .insert(attributes)
+    .returning('*')
+    .then(firstRecord)
+
+
+const updateRecord = (table, id, attributes) =>
+  knex
+    .table(table)
+    .where('id', id)
+    .update(attributes)
+    .returning('*')
+    .then(firstRecord)
+
+const deleteRecord = (table, id) =>
+  knex
+    .table(table)
+    .where('id', id)
+    .del()
+
+
+const removeUserFromBoard = (userId, boardId) =>
+  knex
+    .table('user_boards')
+    .where({'user_id': userId , 'board_id': boardId})
+    .del()
+
+const archiveRecord = (table, id) =>
+  knex
+    .table(table)
+    .where('id', id)
+    .update({
+      archived: true,
+    })
+
+const unarchiveRecord = (table, id) =>
+  knex
+    .table(table)
+    .where('id', id)
+    .update({
+      archived: false,
+    })
+
+const archiveListItems = (id) =>
+  knex
+    .table('cards')
+    .where('list_id', id)
+    .update({
+      archived: true,
+    })
+
+const unarchiveListItems = (id) =>
+  knex
+    .table('cards')
+    .where('list_id', id)
+    .update({
+      archived: false,
+    })
+
+const findOrCreateUserFromGithubProfile = (githubProfile) => {
+  const github_id = githubProfile.id
+  const userAttributes = {
+    github_id: github_id,
+    name: githubProfile.name,
+    email: githubProfile.email,
+    avatar_url: githubProfile.avatar_url,
+  }
+  return knex.table('users').where('github_id', github_id).first('*')
+    .then(user => user ? user : createUser(userAttributes))
+}
+
+const createUser = (attributes) =>
+  createRecord('users', attributes)
+    .then(user => {
+      sendWelcomeEmail(user)
+      return user
+    })
+
+const updateUser = (id, attributes) =>
+  updateRecord('users', id, attributes)
+
+
+const deleteUser = (id) =>
+  deleteRecord('users', id)
+
+
+//
+
+const createList = (attributes) => {
+  return createRecord('lists', attributes)
+}
+
+const updateList = (id, attributes) =>
+  updateRecord('lists', id, attributes)
+
+
+const deleteList = (id) =>
+  Promise.all([
+    deleteRecord('lists', id),
+    knex.table('cards').where('list_id', id).del(),
+  ])
+
+//
+
+const createCard = (attributes) => {
+  return knex
+    .table('cards')
+    .where({list_id: attributes.list_id})
+    .orderBy('order', 'desc')
+    .limit(1)
+    .then( ([result]) => {
+      attributes.order = result ? result.order + 1 : 0
+      return knex
+        .table('cards')
+        .insert(attributes)
+        .returning('*')
+        .then(firstRecord)
+    })
+}
+
+const updateCard = (id, attributes) =>
+  updateRecord('cards', id, attributes)
+
+const deleteCard = (id) =>
+  deleteRecord('cards', id)
+
+const archiveCard = (id) =>
+  archiveRecord('cards', id)
+
+const unarchiveCard = (id) =>
+  unarchiveRecord('cards', id)
+
+const moveCard = ({ boardId, cardId, listId, order }) => {
+  return knex
+    .table('cards')
+    .where({board_id: boardId})
+    .orderBy('order', 'asc')
+    .then(allCards => {
+
+      const cardBegingMoved = allCards.find(card => card.id === cardId)
+      const originListId = cardBegingMoved.list_id
+      const destinationListId = listId
+      const cardsOnOriginList = allCards.filter(card => card.list_id === originListId)
+
+      const updates = []
+
+      updates.push(updateCard(cardBegingMoved.id, {
+        list_id: destinationListId,
+        order: order,
+      }))
+
+      if (originListId === destinationListId){
+        cardsOnOriginList.forEach(card => {
+          if (card !== cardBegingMoved && card.order >= order) {
+            updates.push(updateCard(card.id, {
+              order: card.order + 1,
+            }))
+          }
+        })
+      }else{
+        const cardsOnDestinationList = allCards.filter(card =>
+          card.list_id === destinationListId
+        )
+        cardsOnOriginList.forEach(card => {
+          if (card !== cardBegingMoved && card.order >= cardBegingMoved.order) {
+            updates.push(updateCard(card.id, {
+              order: card.order - 1,
+            }))
+          }
+        })
+        cardsOnDestinationList.forEach(card => {
+          if (card.order >= order) {
+            updates.push(updateCard(card.id, {
+              order: card.order + 1,
+            }))
+          }
+        })
+      }
+
+      return Promise.all(updates)
+    })
+}
+
+const archiveList = (id) =>
+  Promise.all([
+    archiveListItems(id),
+    archiveRecord('lists', id)
+  ])
+
+const unarchiveList = (id) =>
+  Promise.all([
+    unarchiveRecord('lists', id),
+    unarchiveListItems(id)
+  ])
+
+const archiveBoard = (id) =>
+  archiveRecord('boards', id)
+
+const unarchiveBoard = (id) =>
+  unarchiveRecord('boards', id)
+
+const createBoard = (userId, attributes) => {
+  if (!attributes.background_color) delete attributes.background_color
+  return createRecord('boards', attributes).then(board => {
+    let attrs = {
+      user_id: userId,
+      board_id: board.id,
+    }
+    return createRecord('user_boards', attrs).then(() => board)
+  })
+}
+
+const addUserToBoard = (userId, boardId) => {
+  let attrs = {
+    user_id: userId,
+    board_id: boardId,
+  }
+  return knex.table('user_boards')
+    .select('*')
+    .where({
+      user_id: userId,
+      board_id: boardId
+    })
+    .whereNotExists( function() {
+      this.select('*')
+      .from('user_boards')
+      return createRecord('user_boards', attrs)
+    })
+}
+
+
+const updateBoard = (id, attributes) =>
+  updateRecord('boards', id, attributes)
+
+
+const deleteBoard = (boardId) =>
+  Promise.all([
+    deleteRecord('boards', boardId),
+    knex.table('user_boards').where('board_id', boardId).del(),
+  ]).then(results => results[0] + results[1])
+
+// INVITES
+const createInvite = (attributes) =>
+  createRecord('invites', attributes)
+
+const searchQuery = ( userId, searchTerm ) => {
+  return queries.getSearchResult(userId, searchTerm)
+  .then(result => {
+    return result
+  })
+}
+
+export default {
+  createUser,
+  updateUser,
+  deleteUser,
+  findOrCreateUserFromGithubProfile,
+  createList,
+  updateList,
+  deleteList,
+  createCard,
+  updateCard,
+  deleteCard,
+  moveCard,
+  createBoard,
+  updateBoard,
+  deleteBoard,
+  archiveCard,
+  unarchiveCard,
+  archiveList,
+  unarchiveList,
+  archiveBoard,
+  unarchiveBoard,
+  createInvite,
+  addUserToBoard,
+  searchQuery,
+  removeUserFromBoard,
+}
---
+++ oldList.js
@@ -0,0 +1,170 @@
+
+import React, { Component } from 'react'
+import $ from 'jquery'
+import Form from '../Form'
+import Link from '../Link'
+import Icon from '../Icon'
+import Card from './Card'
+import EditCardForm from './EditCardForm'
+import boardStore from '../../stores/boardStore'
+import autosize from 'autosize'
+import ToggleComponent from '../ToggleComponent'
+import Button from '../Button'
+import PopoverMenuButton from '../PopoverMenuButton'
+import ListActionsMenu from '../ListActionsMenu'
+
+
+export default class List extends Component {
+
+  static propTypes = {
+    board: React.PropTypes.object.isRequired,
+    list:  React.PropTypes.object.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      creatingCard: false
+    }
+    this.creatingCard = this.creatingCard.bind(this)
+    this.cancelCreatingCard = this.cancelCreatingCard.bind(this)
+    this.cancelCreatingCardIfUserClickedOutside = this.cancelCreatingCardIfUserClickedOutside.bind(this)
+    document.body.addEventListener('click', this.cancelCreatingCardIfUserClickedOutside)
+  }
+
+  componentWillUnmount(){
+    document.body.removeEventListener('click', this.cancelCreatingCardIfUserClickedOutside)
+  }
+
+  cancelCreatingCardIfUserClickedOutside(event){
+    const targetNode = event.target
+    let rootNode = this.refs.root
+    if (rootNode && targetNode && !rootNode.contains(targetNode))
+      this.cancelCreatingCard()
+  }
+
+  componentDidUpdate(){
+    if (this.state.creatingCard){
+      const { cards } = this.refs
+      cards.scrollTop = cards.scrollHeight
+    }
+  }
+
+  creatingCard() {
+    this.setState({creatingCard: true})
+  }
+
+  cancelCreatingCard() {
+    this.setState({creatingCard: false})
+  }
+
+  render(){
+    const { board, list, dragging } = this.props
+
+    const cards = board.cards
+      .map(card =>
+        dragging && card.id === dragging.cardId ?
+          {...card, order: dragging.order, list_id: dragging.listId} :
+          card
+      )
+      .filter(card => !card.archived)
+      .filter(card => card.list_id === list.id)
+      .sort((a, b) => a.order - b.order)
+
+    const cardNodes = cards.map((card, index) =>
+      <Card
+        editable
+        key={card.id}
+        card={card}
+        index={index}
+        ghosted={dragging && card.id === dragging.cardId}
+        board={board}
+        list={list}
+      />
+    )
+
+    let newCardForm, newCardLink
+    if (this.state.creatingCard) {
+      newCardForm = <NewCardForm
+        board={board}
+        list={list}
+        onCancel={this.cancelCreatingCard}
+      />
+    } else {
+      newCardLink = <Link onClick={this.creatingCard} className="BoardShowPage-create-card-link" >Add a card...</Link>
+    }
+
+    const listActionsMenu = <ListActionsMenu
+      list={this.props.list}
+      onCreateCard={this.creatingCard}
+    />
+
+    return <div className="BoardShowPage-List" data-list-id={list.id}>
+      <div className="BoardShowPage-ListHeader">
+        {list.name}
+        <PopoverMenuButton className="BoardShowPage-ListHeader-ListOptions" type="invisible" popover={listActionsMenu}>
+          <Icon type="ellipsis-h" />
+        </PopoverMenuButton>
+      </div>
+      <div
+        ref="cards"
+        className="BoardShowPage-cards"
+        onDragStart={this.props.onDragStart}
+        onDragOver={this.props.onDragOver}
+        onDragEnd={this.props.onDragEnd}
+        onDrop={this.props.onDrop}
+      >
+        {cardNodes}
+        {newCardForm}
+      </div>
+      {newCardLink}
+    </div>
+  }
+}
+
+
+class NewCardForm extends Component {
+
+  static propTypes = {
+    board: React.PropTypes.object.isRequired,
+    list: React.PropTypes.object.isRequired,
+    onCancel: React.PropTypes.func.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+    this.createCard = this.createCard.bind(this)
+  }
+
+  createCard(card) {
+    const { board, list } = this.props
+    if (card.content.replace(/\s+/g,'') === '') return
+
+    $.ajax({
+      method: 'post',
+      url: `/api/boards/${board.id}/lists/${list.id}/cards`,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      data: JSON.stringify(card),
+    }).then(() => {
+      boardStore.reload()
+    })
+  }
+
+  render() {
+    return <EditCardForm
+      onCancel={this.props.onCancel}
+      submitButtonName="Add"
+      onSave={this.createCard}
+    />
+  }
+}
+
+const archiveRecord = (resource, id) => {
+  $.ajax({
+    method: "POST",
+    url: `/api/${resource}/${id}/archive`
+  }).then(() => {
+    boardStore.reload()
+  })
+}
