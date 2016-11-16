dbb8f0b0b912008235c731720214a2e33a2fc5b6
Amelia Clarke <ameliavoncat@gmail.com>
2016-11-16 15:03:39 -0800

    Http route test for GET /api/invites/verify/:token

--- test/server/api/invites_test.js
+++ test/server/api/invites_test.js
@@ -9,6 +9,7 @@ const getInvites = () =>
   knex.table('invites').select('*')
 
 describe('POST /api/invites/:boardId', () => {
+
   withBoardsListsAndCardsInTheDatabase( () => {
     context('when logged in', () => {
       beforeEach(() => {
@@ -41,6 +42,29 @@ describe('POST /api/invites/:boardId', () => {
             const invitedEmails = response.map(invitee => invitee.email)
             expect(invitedEmails).to.include('larry@harvey.to')
           })
+        })
+      })
+
+      // get board, check if user is added, then check again
+      describe('GET api/invites/verify/:token', () => {
+        it('should add a user if token is verified', () => {
+          return request('get', '/api/boards/')
+            .then( response => {
+              const userBoards = response.body.map(board => board.id)
+              expect(userBoards).to.not.include(104)
+            })
+            .then( () => request('post', '/api/invites/104', {email: 'mark@zuckerburg.io'}))
+            .then( () => getInvites())
+            .then( response => {
+              const invitedTokens = response.map(invitee => invitee.token)
+              return request('get', `/api/invites/verify/${invitedTokens[0]}`)
+            })
+            .then( () => request('get', '/api/boards/') )
+            .then( response => {
+              const userBoards = response.body.map(board => board.id)
+              expect(userBoards).to.include(104)
+            })
+        })
       })
     })
   })
