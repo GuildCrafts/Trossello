c3c54ef8e69699a2ed460eb86129f959740191ed
Shakalee14 <shakalee14@gmail.com>
2016-12-14 18:01:10 -0800

    Selenium tests for MenuSideBar, BoardShowPage-Lists, Homepage

---
+++ test/features/boardshowpage-list_test.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Key from 'selenium-webdriver'
+const { expect } = require('../setup')
+import { By, until, usingSelenium } from '../selenium-helpers'
+import { withBoardsListsAndCardsInTheDatabase } from '../helpers'
+
+
+describe('Board Show Page Tests for Lists', () => {
+  withBoardsListsAndCardsInTheDatabase( () => {
+    usingSelenium(()=> {
+      describe('when a user is logged in', ()=> {
+        beforeEach(function(){
+          return this.loginAs(1455)
+        })
+
+        it('can click on a previously existing list header and edit the header name', function(done){
+          this.timeout((1000 * 60))
+          this.browser.visit('/boards/101')
+          this.browser.wait(until.elementLocated(By.className('BoardShowPage-ListHeader')), 2000).click()
+          this.browser.findElement(By.css('.BoardShowPage-ListHeader > input'), 2000).sendKeys('New List1')
+          this.browser.findElement(By.css('body'), 2000).click()
+          this.browser.wait(until.elementLocated(By.xpath('//div[@class="BoardShowPage-ListHeader" and ./div[contains(.,"New List1")]]')), 2000)
+          this.browser.then(_ => done())
+        })
+
+        it('can create a new list', function(done){
+          this.timeout((1000 * 60))
+          this.browser.visit('/boards/101')
+          this.browser.wait(until.elementLocated(By.className('BoardShowPage-NewListForm-Link'), 2000)).click()
+          this.browser.findElement(By.css('.BoardShowPage-NewListForm-Form > form > input'), 2000).sendKeys('List3')
+          this.browser.findElement(By.className('Button-primary'), 2000).click()
+          this.browser.findElement(By.className('Icon fa fa-times'), 2000).click()
+          this.browser.wait(until.elementLocated(By.xpath('//div[@class="BoardShowPage-ListHeader" and ./div[contains(.,"List3")]]')), 2000)
+          this.browser.then(_ => done())
+        })
+
+        it('can copy a list using the list popover menu', function(done){
+          this.timeout((1000 * 60))
+          this.browser.visit('/boards/101')
+          this.browser.wait(until.elementLocated(By.css('.BoardShowPage-List[data-list-id="40"] .BoardShowPage-ListOptions > button')), 2000).click()
+          this.browser.wait(until.elementLocated(By.className('ListActionsMenu-ListActionsPane-CopyList')), 2000).click()
+          this.browser.wait(until.elementLocated(By.css('.ListActionsMenu-CopyListPane textarea')), 2000).sendKeys('Copy List 1')
+          this.browser.findElement(By.css('.ListActionsMenu-CopyListPane button'), 2000).click()
+          this.browser.wait(until.elementLocated(By.xpath('//div[@class="BoardShowPage-ListHeader" and ./div[contains(.,"Copy List 1")]]')), 2000)
+          this.browser.then(_ => done())
+        })
+      })
+    })
+  })
+})
--- test/features/homepage_test.js
+++ test/features/homepage_test.js
@@ -22,28 +22,28 @@ describe('Homepage tests', () => {
           return this.loginAs(10000)
         })
         it('Should go to the list of boards', function(done){
-          this.timeout(10 * (1000 * 60));
-          this.browser.visit('/');
-          this.browser.wait(until.elementLocated(By.className('LoggedInHomepage-BoardListHeading')), 1000);
+          this.timeout((1000 * 60))
+          this.browser.visit('/')
+          this.browser.wait(until.elementLocated(By.className('LoggedInHomepage-BoardListHeading')), 2000)
           this.browser.findElement(By.className('LoggedInHomepage-BoardListHeading')).getText()
           .then(result => expect(result).to.eql('All Boards'))
           this.browser.then(_ => done())
         })
 
         it('Should create a new board and select that board when you use the boards dropdown', function(done){
-          this.timeout(10 * (1000 * 60))
+          this.timeout((1000 * 60))
           this.browser.visit('/')
           this.browser.findElement(By.className('BoardButton')).click()
-          this.browser.wait(until.elementLocated(By.className('BoardsDropdown-Button')), 2000).click();
+          this.browser.wait(until.elementLocated(By.className('BoardsDropdown-Button')), 2000).click()
           this.browser.findElement(By.className('CreateBoardPopover-boardName-input')).sendKeys('Test Board')
           this.browser.findElement(By.xpath("//div[@color='#9d7cae']")).click()
           this.browser.findElement(By.className('CreateBoardPopover-submit-button')).click()
-          this.browser.wait(until.elementLocated(By.xpath('//div[@class="BoardShowPage-Header" and ./div/button/h1[contains(.,"Test Board")]]')), 5000)
+          this.browser.wait(until.elementLocated(By.xpath('//div[@class="BoardShowPage-Header" and ./div/button/h1[contains(.,"Test Board")]]')), 2000)
           this.browser.then(_ => done())
         })
 
         it('Should logout', function(done){
-          this.timeout(10 * (1000 *  60))
+          this.timeout((1000 *  60))
           this.browser.visit('/')
           this.browser.findElement(By.className('LogoutButton')).click()
           this.browser.findElement(By.className('Button-danger')).click()
@@ -51,7 +51,23 @@ describe('Homepage tests', () => {
           this.browser.then(_ => done())
         })
       })
-    })
 
+      describe('when a user with boards is logged in', () => {
+        beforeEach(function(){
+          return this.loginAs(1455)
+        })
+        it('Should star and then unstar a board', function(done){
+          this.timeout((1000 * 60))
+          this.browser.visit('/')
+          this.browser.wait(until.elementLocated(By.css('.LoggedInHomepage-AllBoards .LoggedInHomepage-Board[data-board-id="101"] > button')), 2000).click()
+          this.browser.wait(until.elementLocated(By.css('.LoggedInHomepage-StarredBoards .LoggedInHomepage-Board[data-board-id="101"]')), 2000)
+            .then(board =>{
+              this.browser.findElement(By.css('.LoggedInHomepage-StarredBoards .LoggedInHomepage-Board[data-board-id="101"] > button'), 2000).click()
+              this.browser.wait(until.stalenessOf(board), 2000)
+              this.browser.then(_ => done())
+            })
+        })
+      })
+    })
   })
 })
