3808398a00df3284405e083184c325ef45948d07
Jared Grippe <jared@deadlyicon.com>
2016-12-20 11:29:52 -0800

    the Form react component now prevents submit by default

--- browser/components/Form.js
+++ browser/components/Form.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react'
 import './Form.sass'
 
 class Form extends Component {
+
+  onSubmit = (event) => {
+    if (!this.props.target) event.preventDefault()
+    if (this.props.onSubmit) this.props.onSubmit(event)
+  }
+
   render(){
     const props = Object.assign({}, this.props)
     props.className = `Form ${props.className||''}`
+    props.onSubmit = this.onSubmit
     return <form {...props}>{this.props.children}</form>
   }
 }
